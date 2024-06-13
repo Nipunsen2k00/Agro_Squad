546511a03829a9615ed868c2743713bfff790ef7
AVPDD6240N <164396076+AVPDD6240N@users.noreply.github.com>
2024-06-13 12:13:59 +0530

    Create About.php

---
+++ FEDAS/About.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us - Farming Services</title>
+    <link rel="stylesheet" href="./css/About.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="Images//Green_and_White_Circle_Icon_Organic_Food_Logo-removebg-preview 1.png" alt="Fedas Logo">
+        </div>
+        <nav>
+        <ul>
+                <li><a href="Home.php">Home</a></li>
+                <li><a href="About.php">About</a></li>
+                <li><a href="Service1.php">Services</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+                <li><a href="Resource.php">Resources</a></li>
+            </ul>
+        </nav>
+        <div class="search-bar">
+            <input type="text" placeholder="Search">
+        </div>
+    </header>
+    <section class="about-hero">
+        <div class="about-content">
+            <h1>About Us</h1>
+            <h2>Who We Are?</h2>
+            <p>Our platform revolutionizes how farmers access agricultural information and resources, empowering them with comprehensive knowledge, best practices, and market insights to enhance their practices.</p>
+            <p>Join us on this journey to transform agriculture for the better. Together, we can build a more sustainable, resilient, and prosperous future for farming communities around the world.</p>
+            <p>Thank you for choosing the FEDAS System!</p>
+        </div>
+    </section>
+</body>
+</html>
